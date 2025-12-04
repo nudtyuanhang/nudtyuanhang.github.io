370a4a014a1db523270af896e9bf8cdd10618725
nudtyuanhang <49294279+nudtyuanhang@users.noreply.github.com>
2025-12-04 06:45:34 +0000

    Deploying to gh-pages from @ nudtyuanhang/nudtyuanhang.github.io@0b9409fc78f4449425a2b23881d943658291e758 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -57,16 +57,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
+            },},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-the-personal-homepage-has-officially-been-launched",
+          title: 'The personal homepage has officially been launched.',
           description: "",
           section: "News",},{
         id: 'social-cv',
